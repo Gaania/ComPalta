e7b11b44cdfd6c9a9a68307d169bbbdbe9775fee
Gania <ganiavin.gv@gmail.com>
2024-04-18 13:39:37 -0300

    avance en el backend del formulario de registro

---
+++ usuario/back/signup.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_POST)){
+    //pasar variables
+    $nombre=$_POST['nombre'];
+    $correo=$_POST['correo'];
+    $clave=$_POST['clave'];
+    $clave2=$_POST['clave2'];
+
+    //validaciones
+    /*if($nombre=="jugador3"){
+        echo "<script>alert('ERROR: Debe ingresar algún tipo de identificación.');window.location=-1;</script>";
+    }*/
+}
+?>
