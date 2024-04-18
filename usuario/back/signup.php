d5e097da58fc8e631439f42925bd288aa7d78aff
gaania <ganiavin.gv@gmail.com>
2024-04-18 18:41:37 -0300

    avance en el backend del formulario de registro

--- usuario/back/signup.php
+++ usuario/back/signup.php
@@ -6,9 +6,21 @@ if (isset($_POST)){
     $clave=$_POST['clave'];
     $clave2=$_POST['clave2'];
 
+    $creacion= date('Y m d');
+
+    echo $creacion;
     //validaciones
-    /*if($nombre=="jugador3"){
-        echo "<script>alert('ERROR: Debe ingresar algún tipo de identificación.');window.location=-1;</script>";
-    }*/
+    if((strlen($nombre)<=3) or (strlen($nombre)>=21)){
+        echo "<script>alert('ERROR: Su nombre de usuario debe tener entre 4 y 20 caracteres.');window.location='';</script>";
+    }
+
+    if((strlen($correo)<=9) or (strlen($correo)>=51)){
+        echo "<script>alert('ERROR: Su correo debe tener entre 10 y 50 caracteres.');window.location='';</script>";
+    }
+
+    if((strlen($clave)<=7) or (strlen($clave)>=13)){
+        echo "<script>alert('ERROR: Su contraseña debe tener entre 8 y 12 caracteres.');window.location='';</script>";
+    }
+
 }
 ?>
