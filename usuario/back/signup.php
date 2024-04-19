e4c92ca42907a63197b247d3ecd8909d9fc2e383
gaania <ganiavin.gv@gmail.com>
2024-04-19 16:37:56 -0300

    Se ha completado el funcionamiento backend del formulario de registro. Se ha agregado una carpeta de imágenes, destinadas a ser íconos para los perfiles de los usuarios

--- usuario/back/signup.php
+++ usuario/back/signup.php
@@ -1,26 +1,68 @@
 <?php
-if (isset($_POST)){
+if (isset($_GET)){
     //pasar variables
-    $nombre=$_POST['nombre'];
-    $correo=$_POST['correo'];
-    $clave=$_POST['clave'];
-    $clave2=$_POST['clave2'];
+    $nombre=$_GET['nombre'];
+    $correo=$_GET['correo'];
+    $clave=$_GET['clave'];
+    $clave2=$_GET['clave2'];
 
-    $creacion= date('Y m d');
+    $fechacreacion= date('Y-m-d');
+    
+    //conexion a la BD
+    include ("../../BDD/conexion.php");
 
-    echo $creacion;
     //validaciones
+    if($clave!=$clave2){
+        echo "<script>alert('ERROR: Las contraseñas no son iguales');window.location='';</script>";
+        exit();
+    }
+
     if((strlen($nombre)<=3) or (strlen($nombre)>=21)){
         echo "<script>alert('ERROR: Su nombre de usuario debe tener entre 4 y 20 caracteres.');window.location='';</script>";
+        exit();
     }
 
     if((strlen($correo)<=9) or (strlen($correo)>=51)){
         echo "<script>alert('ERROR: Su correo debe tener entre 10 y 50 caracteres.');window.location='';</script>";
+        exit();
     }
 
     if((strlen($clave)<=7) or (strlen($clave)>=13)){
         echo "<script>alert('ERROR: Su contraseña debe tener entre 8 y 12 caracteres.');window.location='';</script>";
+        exit();
     }
 
+
+    //validaciones de repetición
+    $query="SELECT * from usuario where nombre='$nombre'";
+    $res=mysqli_query($conexion, $query);
+    if ($res){
+        $num=mysqli_num_rows($res);
+        if($num!=0){
+            echo "<script>alert('ERROR: Este nombre de usuario ya está asociado a una cuenta.');window.location='';</script>";
+            exit();
+        }
+    }
+
+    $query="SELECT * from usuario where correo='$correo'";
+    $res=mysqli_query($conexion, $query);
+    if ($res){
+        $num=mysqli_num_rows($res);
+        if($num!=0){
+            echo "<script>alert('ERROR: Este correo ya está asociado a una cuenta.');window.location='';</script>";
+            exit();
+        }
+    }
+
+    //guardar registro
+    $query="INSERT INTO `usuario`(`nombre`, `clave`, `correo`, `imagen`, `fechaCreacion`) VALUES ('$nombre','$clave','$correo','','$fechacreacion')";
+    $res=mysqli_query($conexion, $query);
+    if (!$res){
+        echo "<script>alert('ERROR: No se guardó su usuario. Vuelva a intentar.');window.location='';</script>";
+        exit();
+    }
+
+    echo "<script>alert('¡Su usuario se ha registrado!');window.location='';</script>";
+    exit();
 }
 ?>
