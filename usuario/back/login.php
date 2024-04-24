a35b63e57f63ac36503515a3045644217cf8300c
Gania <ganiavin.gv@gmail.com>
2024-04-24 16:45:14 -0300

    Formulario de inicio de sesión totalmente funcional. Pequeños cambios estéticos

---
+++ usuario/back/login.php
@@ -0,0 +1,36 @@
+<?php
+
+if(isset($_GET)){
+    //pasar variables
+    $correo=$_GET['correo'];
+    $clave=$_GET['clave'];
+
+    //conexion a la bdd
+    include ("../../BDD/conexion.php");
+
+    //validaciones
+    $query="SELECT * from usuario where correo='$correo'";
+    $res=mysqli_query($conexion, $query);
+    if ($res){
+        $num=mysqli_num_rows($res);
+        if($num==0){
+            echo "<script>alert('ERROR: Este correo no está asociado a una cuenta.');window.location='../vistas/login.html';</script>";
+            exit();
+        }
+    }
+
+    $query="SELECT * from usuario where correo='$correo' and clave='$clave'";
+    $res=mysqli_query($conexion, $query);
+    if ($res){
+        $num=mysqli_num_rows($res);
+        if($num!=0){
+            echo "<script>alert('Has iniciado sesión');window.location='';</script>";
+            exit();
+        }else{
+            echo "<script>alert('ERROR: El correo o contraseña son incorrectos.');window.location='../vistas/login.html';</script>";
+            exit();
+        }
+    }
+
+
+}
--- usuario/back/signup.php
+++ usuario/back/signup.php
@@ -13,22 +13,22 @@ if (isset($_GET)){
 
     //validaciones
     if($clave!=$clave2){
-        echo "<script>alert('ERROR: Las contraseñas no son iguales');window.location='';</script>";
+        echo "<script>alert('ERROR: Las contraseñas no son iguales');window.location='../vistas/signup.html';</script>";
         exit();
     }
 
     if((strlen($nombre)<=3) or (strlen($nombre)>=16)){
-        echo "<script>alert('ERROR: Su nombre de usuario debe tener entre 4 y 15 caracteres.');window.location='';</script>";
+        echo "<script>alert('ERROR: Su nombre de usuario debe tener entre 4 y 15 caracteres.');window.location='../vistas/signup.html';</script>";
         exit();
     }
 
     if((strlen($correo)<=9) or (strlen($correo)>=51)){
-        echo "<script>alert('ERROR: Su correo debe tener entre 10 y 50 caracteres.');window.location='';</script>";
+        echo "<script>alert('ERROR: Su correo debe tener entre 10 y 50 caracteres.');window.location='../vistas/signup.html';</script>";
         exit();
     }
 
     if((strlen($clave)<=7) or (strlen($clave)>=13)){
-        echo "<script>alert('ERROR: Su contraseña debe tener entre 8 y 12 caracteres.');window.location='';</script>";
+        echo "<script>alert('ERROR: Su contraseña debe tener entre 8 y 12 caracteres.');window.location='../vistas/signup.html';</script>";
         exit();
     }
 
@@ -39,7 +39,7 @@ if (isset($_GET)){
     if ($res){
         $num=mysqli_num_rows($res);
         if($num!=0){
-            echo "<script>alert('ERROR: Este nombre de usuario ya está asociado a una cuenta.');window.location='';</script>";
+            echo "<script>alert('ERROR: Este nombre de usuario ya está asociado a una cuenta.');window.location='../vistas/signup.html';</script>";
             exit();
         }
     }
@@ -49,16 +49,17 @@ if (isset($_GET)){
     if ($res){
         $num=mysqli_num_rows($res);
         if($num!=0){
-            echo "<script>alert('ERROR: Este correo ya está asociado a una cuenta.');window.location='';</script>";
+            echo "<script>alert('ERROR: Este correo ya está asociado a una cuenta.');window.location='../vistas/signup.html';</script>";
             exit();
         }
     }
 
     //guardar registro
-    $query="INSERT INTO `usuario`(`nombre`, `clave`, `correo`, `imagen`, `fechaCreacion`) VALUES ('$nombre','$clave','$correo','','$fechacreacion')";
+    $query="INSERT INTO `usuario`(`nombre`, `clave`, `correo`, `imagen`, `fechaCreacion`) 
+    VALUES ('$nombre','$clave','$correo','','$fechacreacion')";
     $res=mysqli_query($conexion, $query);
     if (!$res){
-        echo "<script>alert('ERROR: No se guardó su usuario. Vuelva a intentar.');window.location='';</script>";
+        echo "<script>alert('ERROR: No se guardó su usuario. Vuelva a intentar.');window.location='../vistas/signup.html';</script>";
         exit();
     }
 
